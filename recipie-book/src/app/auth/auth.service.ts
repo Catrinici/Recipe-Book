676aca5655483d32ca2f1232978cdd3454df261f
Catrinici <catrinici.antonina@gmail.com>
2020-05-20 17:03:21 -0700

    added auth guard

--- recipie-book/src/app/auth/auth.service.ts
+++ recipie-book/src/app/auth/auth.service.ts
@@ -3,6 +3,8 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators'
 import { throwError, BehaviorSubject } from 'rxjs';
 import { User } from './user.model';
+import { Router } from '@angular/router';
+import { stringify } from 'querystring';
 
 export interface AuthResponseData {
 	kind: string;
@@ -17,8 +19,9 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
 	user = new BehaviorSubject<User>(null);
+	private tokenExpirationTimer: any;
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private router: Router) { }
 
 	signup(email: string, password: string) {
 		return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCslY-79KWVviraGEfHO7tfnzp74rd08X4',
@@ -52,32 +55,78 @@ export class AuthService {
 				));
 	}
 
-	logout() {
-		this.user.next(null);
+	autoLogin() {
+		const userData: {
+			email: string;
+			id: string;
+			_token: string;
+			_tokenExpirationDate: string;
+		} = JSON.parse(localStorage.getItem('userData'));
+		if (!userData) {
+			return;
+		}
+
+		const loadedUser = new User(
+			userData.email,
+			userData.id,
+			userData._token,
+			new Date(userData._tokenExpirationDate)
+		);
+
+		if (loadedUser.token) {
+			this.user.next(loadedUser);
+			const expirationDuration =
+				new Date(userData._tokenExpirationDate).getTime() -
+				new Date().getTime();
+			this.autoLogout(expirationDuration);
+		}
 	}
 
-	private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+	logout() {
+		this.user.next(null);
+		this.router.navigate(['/auth']);
+		localStorage.removeItem('userData');
+		if (this.tokenExpirationTimer) {
+			clearTimeout(this.tokenExpirationTimer);
+		}
+		this.tokenExpirationTimer = null;
+	}
+
+	autoLogout(expirationDuration: number) {
+		this.tokenExpirationTimer = setTimeout(() => {
+			this.logout();
+		}, expirationDuration);
+	}
+
+	private handleAuthentication(
+		email: string,
+		userId: string,
+		token: string,
+		expiresIn: number
+	) {
 		const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
 		const user = new User(email, userId, token, expirationDate);
 		this.user.next(user);
+		this.autoLogout(expiresIn * 1000);
+		localStorage.setItem('userData', JSON.stringify(user));
 	}
 
 	private handleError(errorRes: HttpErrorResponse) {
-
 		let errorMessage = 'An unknown error occurred!';
 		if (!errorRes.error || !errorRes.error.error) {
 			return throwError(errorMessage);
 		}
 		switch (errorRes.error.error.message) {
-			case 'EMAIL_EXISTS': errorMessage = 'This email exists already';
+			case 'EMAIL_EXISTS':
+				errorMessage = 'This email exists already';
 				break;
-			case 'EMAIL_NOT_FOUND': errorMessage = "This email does not exist";
+			case 'EMAIL_NOT_FOUND':
+				errorMessage = 'This email does not exist.';
 				break;
-			case 'INVALID_PASSWORD': errorMessage = "This password is not correct";
+			case 'INVALID_PASSWORD':
+				errorMessage = 'This password is not correct.';
 				break;
 		}
-
 		return throwError(errorMessage);
 	}
-
 }
